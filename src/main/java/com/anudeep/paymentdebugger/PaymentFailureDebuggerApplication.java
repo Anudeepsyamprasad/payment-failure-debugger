900deb209773b9f15f8e481a65c9e3f3d1fabce3
Anudeepsyamprasad <anudeeprocky143@gmail.com>
2026-03-04 23:23:00 +0530

    Initial Spring Boot project setup using Maven

---
+++ src/main/java/com/anudeep/paymentdebugger/PaymentFailureDebuggerApplication.java
@@ -0,0 +1,13 @@
+package com.anudeep.paymentdebugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentFailureDebuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentFailureDebuggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anudeep/paymentdebugger/PaymentFailureDebuggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anudeep.paymentdebugger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentFailureDebuggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
